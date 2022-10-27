27564c598821b14be737b7e198ef4e4f5d4f24bc
WONWOOLEE <lww0407@naver.com>
2022-10-27 15:19:00 +0900

    feat: dependencies Added

--- FE/alertyou/.eslintrc.js
+++ FE/alertyou/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  extends: ['prettier'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   overrides: [
---
+++ FE/alertyou/screens/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const LoginScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  )
+}
+
+export default LoginScreen
