38daeebe1baca5ad048edc67c2c12deb35298035
WONWOOLEE <lww0407@naver.com>
2022-10-28 16:24:41 +0900

    feat: 네비게이션 세팅 완료

--- FE/alertyou/src/screens/LoginScreen/LoginScreen.tsx
+++ FE/alertyou/src/screens/LoginScreen/LoginScreen.tsx
@@ -1,28 +1,28 @@
 import {View, Button, Text} from 'react-native';
 import React from 'react';
-// import {tokenState} from '@/store';
-// import {useRecoilState} from 'recoil';
+import {tokenState} from '@/store';
+import {useRecoilState} from 'recoil';
 
 type Props = {
   navigation: any;
 };
-
-// const [tmpToken, setTmpToken] = useRecoilState(tokenState)
 
 // const createTmpToken = () => {
 //   setTmpToken('1');
 // }
 
 const LoginScreen = ({navigation}: Props) => {
+  const [tmpToken, setTmpToken] = useRecoilState(tokenState);
   return (
     <View>
       <Text>LoginHello</Text>
       <Button
         title="회원가입"
-        onPress={() => navigation.navigate('SignUpScreen')}/>
-      {/* <Button 
+        onPress={() => navigation.navigate('SignUpScreen')}
+      />
+      <Button 
         title="토큰부여"
-        onPress={createTmpToken}/> */}
+        onPress={() => setTmpToken("hhihihi")}/>
     </View>
   );
 };
--- FE/alertyou/src/screens/ProfileScreen/ProfileScreen.tsx
+++ FE/alertyou/src/screens/ProfileScreen/ProfileScreen.tsx
@@ -1,16 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { useRecoilState } from 'recoil';
+import { tokenState } from '@/store';
 
 type Props = {
   navigation: any
 }
 
 const ProfileScreen = ({ navigation }: Props) => {
+  const [tmpToken, setTmpToken] = useRecoilState(tokenState);
   return (
     <View>
       <Text>profileScreen</Text>
+      <Button title="로그인페이지 이동" onPress={() => setTmpToken('')} />
     </View>
-  )
+  );
 }
 
 export default ProfileScreen
