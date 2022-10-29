06937ddec647bf9252de8674af318584f6c17680
CodeAlpacat <magicmars123@gmail.com>
2022-10-29 14:47:57 +0900

    feat: form designing

--- FE/alertyou/src/screens/LoginScreen/LoginScreen.tsx
+++ FE/alertyou/src/screens/LoginScreen/LoginScreen.tsx
@@ -1,7 +1,11 @@
-import {View, Button, Text, TextInput} from 'react-native';
+import {View, Button} from 'react-native';
+import {FormControl, Icon, Input, Stack} from 'native-base';
 import React from 'react';
 import {tokenState} from '@/store';
 import {useSetRecoilState} from 'recoil';
+
+import {styles} from './style';
+import {COLOR} from '@/theme/colorVariants';
 
 type Props = {
   navigation: any;
@@ -11,13 +15,31 @@ const LoginScreen = ({navigation}: Props) => {
   const setTmpToken = useSetRecoilState(tokenState);
   const createTmpToken = () => {
     setTmpToken('1');
-  }
+  };
   return (
-    <View>
-      <TextInput/>
-      <Button 
-        title="토큰부여"
-        onPress={createTmpToken}/>
+    <View style={styles.container}>
+      <Stack space={3}>
+        <FormControl isRequired>
+          <Input
+            variant="underlined"
+            placeholder="휴대폰 번호"
+            keyboardType="numeric"
+            size="sm"
+            color={COLOR.mainFont}
+            focusOutlineColor={COLOR.red}
+          />
+          <Input
+            variant="underlined"
+            placeholder="비밀번호"
+            size="sm"
+            color={COLOR.mainFont}
+            focusOutlineColor={COLOR.red}
+            type="password"
+          />
+          <Button title="로그인" onPress={createTmpToken} />
+        </FormControl>
+      </Stack>
+      <Button title="토큰부여" onPress={createTmpToken} />
     </View>
   );
 };
---
+++ FE/alertyou/src/theme/colorVariants.ts
@@ -0,0 +1,8 @@
+export const COLOR = {
+  red: "#ff875C",
+  blue: "#7979F7",
+  mainFont: "#555555",
+  subFont: "#777777",
+  placeholder: "#96989D",
+  lightGrey: "#DFDFDF",
+}
