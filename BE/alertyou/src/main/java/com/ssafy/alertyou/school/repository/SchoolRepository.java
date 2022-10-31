6f18c8b03c74a04cb203c5470834c112a153b389
hyeon <lhynjn9@gmail.com>
2022-10-31 15:52:21 +0900

    feat: school reposiotry 추가, report entity 수정

---
+++ BE/alertyou/src/main/java/com/ssafy/alertyou/school/repository/SchoolRepository.java
@@ -0,0 +1,13 @@
+package com.ssafy.alertyou.school.repository;
+
+import com.ssafy.alertyou.account.entity.User;
+import com.ssafy.alertyou.proof.entity.Proof;
+import com.ssafy.alertyou.school.entity.School;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SchoolRepository extends JpaRepository<School,Long> {
+//    Optional<School> findByNameAndGradeAndRoom();
+}
