331fd015a72c6656404339dc5648f40888279eda
CodeAlpacat <magicmars123@gmail.com>
2022-11-01 00:58:45 +0900

    feat: 검색바 UI 개선

--- FE/alertyou/src/screens/SearchSchoolScreen/SearchSchoolScreen.tsx
+++ FE/alertyou/src/screens/SearchSchoolScreen/SearchSchoolScreen.tsx
@@ -1,26 +1,36 @@
 import {View, Text, Pressable} from 'react-native';
 import React from 'react';
-import {Input, SearchIcon} from 'native-base';
+import {Divider, Flex, Input, SearchIcon} from 'native-base';
 import {MAIN} from '@/theme/colorVariants';
+import {styles} from './style';
 
 const SearchSchoolScreen = () => {
+  
   return (
-    <View>
-      <Pressable onPress={() => {}}>
+    <View style={styles.container}>
+      <View style={styles.searchContainer}>
         <Input
           type="text"
-          variant="filled"
-          editable={false}
+          // rounded="md"
+          variant="underlined"
           placeholder="ex) 싸피고등학교"
           size="md"
-          h="9"
           color={MAIN.mainFont}
           focusOutlineColor={MAIN.red}
-          InputRightElement={<SearchIcon size="md"/>}
+          InputRightElement={<SearchIcon mr="3" size="md" />}
           autoCorrect={false}
-          // value={val}
+          onSubmitEditing={() => {}}
         />
-      </Pressable>
+      </View>
+      <View style={styles.schoolListContainer}>
+        <View>
+          <Flex direction='row' w="100%">
+            <Text>학교명</Text>
+            <Divider orientation='vertical' mx='2'/>
+            <Text>주소</Text>
+          </Flex>
+        </View>
+      </View>
     </View>
   );
 };
