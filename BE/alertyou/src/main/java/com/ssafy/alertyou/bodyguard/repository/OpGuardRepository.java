7b50557aebc0eda988ee0f0da1521fefec3993be
hyeon <lhynjn9@gmail.com>
2022-11-01 15:01:51 +0900

    feat: 신고 api 끝 ㅎㅅㅎ

--- BE/alertyou/src/main/java/com/ssafy/alertyou/bodyguard/repository/OpGuardRepository.java
+++ BE/alertyou/src/main/java/com/ssafy/alertyou/bodyguard/repository/OpGuardRepository.java
@@ -1,11 +1,15 @@
 package com.ssafy.alertyou.bodyguard.repository;
 
 import com.ssafy.alertyou.account.entity.User;
+import com.ssafy.alertyou.bodyguard.entity.Coguard;
 import com.ssafy.alertyou.bodyguard.entity.Opguard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OpGuardRepository extends JpaRepository<Opguard, Long> {
     public List<Opguard> findAllByUser(User user);
+
+    public Optional<Opguard> findByOpGuardAndUser(User opGuard, User user);
 }
