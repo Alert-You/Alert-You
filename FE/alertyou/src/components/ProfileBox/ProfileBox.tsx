d9f24a712ef2265a9e72ac725e3ee616de427a11
WONWOOLEE <lww0407@naver.com>
2022-11-02 17:52:14 +0900

    feat: 마이페이지 UI 제작중

--- FE/alertyou/src/components/ProfileBox/ProfileBox.tsx
+++ FE/alertyou/src/components/ProfileBox/ProfileBox.tsx
@@ -1,18 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { styles } from './style';
-import { Icon } from 'native-base';
+import {View, Text} from 'react-native';
+import React from 'react';
+import {styles} from './style';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {MAIN} from '@/theme/colorVariants';
 
-
-type Props = {}
+type Props = {};
 
 const ProfileBox = (props: Props) => {
   return (
     <View style={styles.container}>
-      <Text>ProfileBox</Text>
-      <Icon/>
+      <View style={styles.contentContainer}>
+        <MaterialCommunityIcons
+          name="map-marker"
+          size={25}
+          color={MAIN.mainFont}
+        />
+        <Text style={styles.contentText}>hello</Text>
+      </View>
+      <View style={styles.contentContainer}>
+        <MaterialCommunityIcons
+          name="account"
+          size={25}
+          color={MAIN.mainFont}
+        />
+        <Text style={styles.contentText}>hello</Text>
+      </View>
+      <View style={styles.contentContainer}>
+        <MaterialCommunityIcons name="phone" size={25} color={MAIN.mainFont} />
+        <Text style={styles.contentText}>hello</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default ProfileBox
+export default ProfileBox;
--- FE/alertyou/src/components/ProfileBox/style.ts
+++ FE/alertyou/src/components/ProfileBox/style.ts
@@ -4,11 +4,25 @@ import { Dimensions, StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-    width: Dimensions.get('window').width-32,
+    width: Dimensions.get('window').width - 32,
     elevation: 6,
     height: 205,
     backgroundColor: WHITE.white,
-    padding: 22,
-    borderRadius: 10
-  }
-})
+    borderRadius: 10,
+    paddingVertical: 7,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  contentContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+    paddingHorizontal: 18,
+  },
+  contentText: {
+    marginLeft: 7,
+    fontSize: 20,
+  },
+});
