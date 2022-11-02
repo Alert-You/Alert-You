b48c291a4209d26378fad5e989b035d7c3687550
Sang Jin <tkdwls8412@naver.com>
2022-11-02 12:19:12 +0900

    feat: 알림내역 디테일 페이지 생성

---
+++ FE/alertyou/src/screens/NoticeScreen/NoticeMap.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'native-base'
+
+const NoticeMap = () => {
+  return (
+    <View>
+      <Text>알림 디테일 페이지</Text>
+    </View>
+  )
+}
+
+export default NoticeMap
