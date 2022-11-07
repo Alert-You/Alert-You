f987b80410189b07b4450ad7f8e06b2c848ab13a
CodeAlpacat <magicmars123@gmail.com>
2022-11-07 23:37:11 +0900

    feat: 네비게이션 디자인

--- FE/alertyou/src/navigations/MainNavigation/MainNavigation.tsx
+++ FE/alertyou/src/navigations/MainNavigation/MainNavigation.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {useRecoilValue, useSetRecoilState} from 'recoil';
@@ -14,6 +14,8 @@ import {SplashScreen} from '@/screens';
 import {getToken} from '@/utils/auth';
 import {useRefreshToken} from '@/hooks';
 import {splashState} from '@/store/splashState';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { MAIN } from '@/theme/colorVariants';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -22,7 +24,7 @@ const MainNavigation = () => {
   const {mutate: refreshMutate} = useRefreshToken();
   const appLoaded = useRecoilValue(splashState);
   const setSplashState = useSetRecoilState(splashState);
-  
+
   //자동로그인 로직
   useEffect(() => {
     getToken().then(token => {
@@ -39,24 +41,61 @@ const MainNavigation = () => {
 
   //토큰 존재 여부 판단
   const isLoggedIn = useRecoilValue(isLoggedInState);
-  return !appLoaded ? <SplashScreen/>  : (
+  return !appLoaded ? (
+    <SplashScreen />
+  ) : (
     <>
       {isLoggedIn ? (
-        <Tab.Navigator initialRouteName="Home">
+        <Tab.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            tabBarActiveTintColor: MAIN.red,
+            tabBarShowLabel: false,
+            tabBarHideOnKeyboard: true,
+            tabBarStyle: {height: 60}
+          }}
+          >
           <Tab.Screen
             name="Notice"
             component={NoticeNavigation}
-            options={{headerShown: false}}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({color}) => (
+                <MaterialCommunityIcons
+                  color={color}
+                  size={35}
+                  name="bell-alert-outline"
+                />
+              ),
+            }}
           />
           <Tab.Screen
             name="Home"
             component={HomeNavigation}
-            options={{headerShown: false}}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({color}) => (
+                <MaterialCommunityIcons
+                  color={color}
+                  size={35}
+                  name="home-outline"
+                />
+              ),
+            }}
           />
           <Tab.Screen
             name="Profile"
             component={ProfileNavigation}
-            options={{headerShown: false}}
+            options={{
+              headerShown: false,
+              tabBarIcon: ({color}) => (
+                <MaterialCommunityIcons
+                  color={color}
+                  size={35}
+                  name="account-outline"
+                />
+              ),
+            }}
           />
         </Tab.Navigator>
       ) : (
