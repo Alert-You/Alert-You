55568004bfb05e2b7cc5d9ffeb7d9a723642c7c4
Sang Jin <tkdwls8412@naver.com>
2022-11-07 13:04:32 +0900

    feat: 학생 상세 페이지 제작

--- FE/alertyou/src/screens/index.ts
+++ FE/alertyou/src/screens/index.ts
@@ -3,7 +3,7 @@ import PhoneAuthScreen from './PhoneAuthScreen/PhoneAuthScreen';
 export { LoginScreen } from './LoginScreen';
 export { SignUpScreen } from './SignUpScreen';
 export { HomeScreen } from './HomeScreen';
-export { ProfileScreen, StudentList } from './ProfileScreen';
+export { ProfileScreen, StudentList, ReportList, ProofList } from './ProfileScreen';
 export { NoticeScreen } from './NoticeScreen';
 export { SignUpSubScreen } from './SignUpSubScreen';
 export { PhoneAuthScreen } from './PhoneAuthScreen';
