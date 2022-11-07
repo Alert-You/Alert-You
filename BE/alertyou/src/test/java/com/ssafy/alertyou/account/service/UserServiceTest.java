591b0a4c3f0e2aa90534df9d6f7786f005c4ed61
SSAFY <zow777@naver.com>
2022-11-07 15:49:35 +0900

    test: 로그인/회원가입 테스트 코드 작성

--- BE/alertyou/src/test/java/com/ssafy/alertyou/account/service/UserServiceTest.java
+++ BE/alertyou/src/test/java/com/ssafy/alertyou/account/service/UserServiceTest.java
@@ -47,4 +47,55 @@ public class UserServiceTest {
 //        assertEquals(user.getUsername(), "박시원");
     }
 
+    @Test
+    @DisplayName("유저 수정 테스트")
+    public void modifyUserTest() {
+        // given
+        UserSignupReqDto userSignupReqDto = new UserSignupReqDto("01092458873", "asd123", "박시원", 1L);
+        userService.createUser(userSignupReqDto);
+
+        // when
+        UserSignupReqDto userModifyReqDto = new UserSignupReqDto("01092458873", "asd123", "시원박", 3L);
+        userService.modifyUserInfo(userRepository.findByPhone(userSignupReqDto.getPhone()), userModifyReqDto);
+
+        // then
+        User user = userRepository.findByPhone(userModifyReqDto.getPhone());
+        assertThat(user.getUsername()).isEqualTo("시원박");
+    }
+    
+    @Test
+    @DisplayName("유저 수정 실패 테스트")
+    public void modifyUserFailTest() {
+        // given
+        UserSignupReqDto userSignupReqDto = new UserSignupReqDto("01092458873", "asd123", "박시원", 1L);
+        userService.createUser(userSignupReqDto);
+
+        UserSignupReqDto userSignupReqDto2 = new UserSignupReqDto("01092458874", "asd123", "원시박", 1L);
+        userService.createUser(userSignupReqDto2);
+
+        // when
+        UserSignupReqDto userModifyReqDto = new UserSignupReqDto("01092458874", "asd123", "시원박", 1L);
+        userService.modifyUserInfo(userRepository.findByPhone(userSignupReqDto.getPhone()), userModifyReqDto);
+
+        // then
+        User user = userRepository.findByPhone(userModifyReqDto.getPhone());
+        assertThat(user.getUsername()).isEqualTo("원시박"); // 유저 수정이 되지 않았으니 원래 주인인 원시박이어야함
+    }
+    
+
+    @Test
+    @DisplayName("유저 탈퇴 테스트")
+    public void deleteUserTest() {
+        // given
+        UserSignupReqDto userSignupReqDto = new UserSignupReqDto("01092458873", "asd123", "박시원", 1L);
+        userService.createUser(userSignupReqDto);
+
+        // when
+        User user = userRepository.findByPhone(userSignupReqDto.getPhone());
+        userService.removeUser(user);
+
+        // then
+        assertThat(user.isActive()).isFalse();
+    }
+
 }
