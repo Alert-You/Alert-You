28b54476cd49d55462fa241d3ac8ff60ea75dae7
WONWOOLEE <lww0407@naver.com>
2022-11-08 17:46:37 +0900

    feat: 위젯 xml 수정

--- FE/alertyou/android/app/src/main/java/com/alertyou/MainApplication.java
+++ FE/alertyou/android/app/src/main/java/com/alertyou/MainApplication.java
@@ -31,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
           new SplashScreenReactPackage();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new CameraRollPackage());
+            packages.add(new SharedStoragePackager());
           return packages;
         }
 
