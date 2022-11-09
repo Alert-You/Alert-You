22e341b515ed2844fb42d7b6627e066963644ee1
Sang Jin <tkdwls8412@naver.com>
2022-11-09 14:42:39 +0900

    refactor: 알림페이지 리팩토링

--- FE/alertyou/src/screens/NoticeScreen/NoticeList/NoticeItem.tsx
+++ FE/alertyou/src/screens/NoticeScreen/NoticeList/NoticeItem.tsx
@@ -8,27 +8,23 @@ import { FlatListItem } from '@/components'
 import { WHITE, RED } from '@/theme/colorVariants'
 import { useNavigation, NavigationProp } from '@react-navigation/native'
 import { NoticeParamList } from '@/navigations/NoticeNavigation/NoticeNavigation'
-import { checkNotice } from '../api'
 import { useMutation } from '@tanstack/react-query'
+
+import { checkNotice } from '../api'
 
 const NoticeItem: React.FC<{ item: noticeItemType; read: boolean }> = ({ item, read }) => {
   const navigation = useNavigation<NavigationProp<NoticeParamList>>()
 
   let notice: string = ''
-  let uri: string = ''
   let bgColor: string = WHITE.white
   if (item.isVictim) {
     notice = '구조요청이 도착했습니다!'
-    uri = "https://images.unsplash.com/photo-1544027993-37dbfe43562a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-
   } else if (!item.isVictim) {
     notice = '목격자 제보가 도착했습니다!'
-    uri = "https://images.unsplash.com/photo-1435348773030-a1d74f568bc2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-
   }
   const date = new Date()
-
   const writeDay = new Date(item.noticeDateTime)
+
   let dif = date.getTime() - writeDay.getTime()
   dif = Math.trunc(dif / 1000);
   // 초 
@@ -48,7 +44,7 @@ const NoticeItem: React.FC<{ item: noticeItemType; read: boolean }> = ({ item, r
     subTitle = Math.trunc(dif / hour) + '시간 전 ';
     //일보다 작으면 몇시간전인지 
   } else {
-    subTitle = item.noticeDateTime
+    subTitle = writeDay.getFullYear() + '년 ' + (writeDay.getMonth() + 1) + '월 ' + writeDay.getDate() + '일 ' + writeDay.getHours() + '시 ' + writeDay.getMinutes() + '분'
   }
 
   const checkMutation = useMutation(checkNotice, {
