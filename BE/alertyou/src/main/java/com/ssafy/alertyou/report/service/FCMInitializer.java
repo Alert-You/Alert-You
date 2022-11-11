b91f348bfedc3cefe20879e3742c238c552a5538
SSAFY <zow777@naver.com>
2022-11-11 16:16:43 +0900

    feat: fcm 기능 구현

---
+++ BE/alertyou/src/main/java/com/ssafy/alertyou/report/service/FCMInitializer.java
@@ -0,0 +1,37 @@
+package com.ssafy.alertyou.report.service;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.InputStream;
+
+@Component
+@Slf4j
+public class FCMInitializer {
+    @Value("${fcm.certification}")
+    private String credential;
+
+    @PostConstruct
+    public void initialize(){
+        ClassPathResource resource = new ClassPathResource(credential);
+
+        try (InputStream stream = resource.getInputStream()) {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(stream))
+                    .build();
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                log.info("FirebaseApp initialization complete");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
