52bcb7d71391fd5dbb386185b0ebab8ba1b29761
Sang Jin <tkdwls8412@naver.com>
2022-11-14 11:06:49 +0900

    feat: 푸시알림 구현

--- FE/alertyou/App.tsx
+++ FE/alertyou/App.tsx
@@ -1,12 +1,24 @@
-import React, {useEffect} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {RecoilRoot} from 'recoil';
-import {MainNavigation} from '@/navigations';
-import {NativeBaseProvider} from 'native-base';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import React, { useEffect } from 'react';
 import Toast from 'react-native-toast-message';
 import SplashScreen from 'react-native-splash-screen';
+import { AppRegistry } from 'react-native';
+import { RecoilRoot } from 'recoil';
+import { NativeBaseProvider } from 'native-base';
 
+import { MainNavigation } from '@/navigations';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import messaging from '@react-native-firebase/messaging';
+import { NavigationContainer } from '@react-navigation/native';
+import pushNoti from '@/utils/pushNoti';
+import notifee, { AndroidImportance, AndroidColor } from '@notifee/react-native';
+
+messaging().setBackgroundMessageHandler(async (remoteMessage: any) => {
+  pushNoti.displayNoti(remoteMessage);
+});
+
+notifee.onBackgroundEvent(async (message: any) => {
+  console.log("nofifee message: ", message.detail.notification)
+});
 
 const queryClinet = new QueryClient({
   defaultOptions: {
@@ -16,10 +28,21 @@ const queryClinet = new QueryClient({
   },
 });
 
+AppRegistry.registerComponent('app', () => App);
+
 const App: React.FunctionComponent = () => {
 
   useEffect(() => {
     SplashScreen.hide();
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log(remoteMessage);
+      pushNoti.displayNoti(remoteMessage);
+    });
+
+    return unsubscribe;
   }, []);
 
   return (
