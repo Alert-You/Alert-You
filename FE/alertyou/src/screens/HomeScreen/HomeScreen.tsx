d97c45268889f556aaaf8bf9f1790f7f634a445f
Sang Jin <tkdwls8412@naver.com>
2022-11-14 11:07:24 +0900

    feat: fcm 토큰 저장 로직 구현

--- FE/alertyou/src/screens/HomeScreen/HomeScreen.tsx
+++ FE/alertyou/src/screens/HomeScreen/HomeScreen.tsx
@@ -1,6 +1,9 @@
-import {useState} from 'react';
-import {Box, ScrollView} from 'native-base';
-import {styles} from './style';
+import { useEffect, useState } from 'react';
+import { Box, ScrollView } from 'native-base';
+
+import messaging from '@react-native-firebase/messaging';
+
+import { styles } from './style';
 
 import {
   HomeTitle,
@@ -9,17 +12,39 @@ import {
   ReportBtns,
   ToggleBtn,
 } from '@/screens/HomeScreen/components';
-import {emergencyBgStyle, nonEmergencyBgStyle} from '@/theme/Home/gradient';
+import { emergencyBgStyle, nonEmergencyBgStyle } from '@/theme/Home/gradient';
+import { saveToken } from './api';
 
 type Props = {
   navigation: any;
 };
 
-const HomeScreen = ({navigation}: Props) => {
+const HomeScreen = ({ navigation }: Props) => {
   let [isEmergency, setIsEmergency] = useState(true);
 
   const toggleIsEmergency = () => {
     setIsEmergency((emergency: boolean) => !emergency);
+  };
+
+  useEffect(() => {
+    requestUserPermission();
+  }, []);
+
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled = authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      return getToken();
+    }
+  };
+
+  const getToken = async () => {
+    const fcmToken = await messaging().getToken();
+    if (fcmToken) {
+      saveToken(fcmToken)
+    }
   };
 
   return (
@@ -27,7 +52,7 @@ const HomeScreen = ({navigation}: Props) => {
       <Box
         bg={isEmergency ? emergencyBgStyle : nonEmergencyBgStyle}
         style={styles.innerContainer}
-        >
+      >
         <HomeTitle
           content={isEmergency ? '긴급 도움 요청' : '현장 목격 신고'}
         />
@@ -36,7 +61,7 @@ const HomeScreen = ({navigation}: Props) => {
           toggleIsEmergency={toggleIsEmergency}
           isEmergency={isEmergency}
         />
-        {isEmergency ? <HelpText /> : <ReportBtns navigation={navigation}/>}
+        {isEmergency ? <HelpText /> : <ReportBtns navigation={navigation} />}
       </Box>
     </ScrollView>
   );
