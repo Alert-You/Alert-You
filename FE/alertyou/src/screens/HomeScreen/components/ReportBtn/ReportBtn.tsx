0ec166e6a2485dfda0de288e9ae9dcccafda754c
박승훈 <tmdgns1126@naver.com>
2022-11-14 14:26:51 +0900

    style: 목격자 기능 버튼 텍스트-버튼 간격 소폭 증가

--- FE/alertyou/src/screens/HomeScreen/components/ReportBtn/ReportBtn.tsx
+++ FE/alertyou/src/screens/HomeScreen/components/ReportBtn/ReportBtn.tsx
@@ -2,7 +2,7 @@ import {innerGradientStyle, outerGradientStyle} from '@/theme/gradient';
 import {AspectRatio, Center, Pressable, VStack} from 'native-base';
 import React from 'react';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { MAIN } from '@/theme/colorVariants'
+import {MAIN} from '@/theme/colorVariants';
 
 interface propsType {
   name: string;
@@ -11,28 +11,21 @@ interface propsType {
 }
 
 const ReportBtn = ({name, nameKr, moveScreen}: propsType) => {
-
   return (
-    <VStack width='28%' space={2}>
+    <VStack width="28%">
       <Pressable onPress={moveScreen}>
-      <AspectRatio
-      width='100%'
-        ratio={{
-          base: 1 / 1,
-        }}
-        
-        >
-        <Center rounded="3xl" bg={outerGradientStyle}>
-          <Center size="85%" rounded="full" bg={innerGradientStyle}>
-          <MaterialCommunityIcons
+        <AspectRatio width="100%" ratio={1 / 1}>
+          <Center rounded="3xl" bg={outerGradientStyle}>
+            <Center size="85%" rounded="full" bg={innerGradientStyle}>
+              <MaterialCommunityIcons
                 name={name}
                 color={MAIN.placeholder}
                 size={50}
               />
+            </Center>
           </Center>
-        </Center>
-      </AspectRatio>
-      <Center _text={{color:'white', fontSize:'lg'}}>{nameKr}</Center>
+        </AspectRatio>
+        <Center mt={2} _text={{color: 'white', fontSize: 'lg'}}>{nameKr}</Center>
       </Pressable>
     </VStack>
   );
