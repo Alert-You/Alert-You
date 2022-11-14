31366f21d91d433e491f72e2a5d0bc89f0ec37a1
박승훈 <tmdgns1126@naver.com>
2022-11-14 13:47:21 +0900

    feat: 홈화면 토글 아이콘 변경

--- FE/alertyou/src/screens/HomeScreen/components/ToggleBtn/ToggleBtn.tsx
+++ FE/alertyou/src/screens/HomeScreen/components/ToggleBtn/ToggleBtn.tsx
@@ -1,15 +1,15 @@
 import {
   AspectRatio,
   Center,
-  Circle,
   HStack,
   PresenceTransition,
   Pressable,
 } from 'native-base';
 import {emergencyBgStyle, nonEmergencyBgStyle} from '@/theme/Home/gradient';
 import React from 'react';
-import {Dimensions} from "react-native"
-import { MAIN } from '@/theme/colorVariants';
+import {Dimensions} from 'react-native';
+import {MAIN} from '@/theme/colorVariants';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 interface propsType {
   toggleIsEmergency: any;
@@ -19,17 +19,22 @@ interface propsType {
 const ToggleBtn = ({toggleIsEmergency, isEmergency}: propsType) => {
   const calcToggleMove = () => {
     const WIDTH = Dimensions.get('window').width;
-    const WIDTH_INNER = WIDTH-32;
-    const TOGGLE_OUTER_WIDTH = WIDTH_INNER*0.4;
-    return TOGGLE_OUTER_WIDTH*0.6;
-  }
+    const WIDTH_INNER = WIDTH - 32;
+    const TOGGLE_OUTER_WIDTH = WIDTH_INNER * 0.4;
+    return TOGGLE_OUTER_WIDTH * 0.6;
+  };
   const toggleMove = calcToggleMove();
 
   return (
     <Center my={3} mb={9}>
       <Pressable onPress={toggleIsEmergency}>
-        <AspectRatio ratio={5 / 2} width="40%" bg={MAIN.lightGrey} rounded="full">
+        <AspectRatio
+          ratio={5 / 2}
+          width="40%"
+          bg={MAIN.lightGrey}
+          rounded="full">
           <HStack width="100%" height="100%" p="5px" alignItems="center">
+            {/* <HStack style={styles.container}> */}
             <PresenceTransition
               visible={!isEmergency}
               initial={{
@@ -38,20 +43,28 @@ const ToggleBtn = ({toggleIsEmergency, isEmergency}: propsType) => {
               }}
               animate={{
                 translateX: toggleMove,
-                rotate: '270deg',
+                rotate: '220deg',
                 transition: {
                   duration: 250,
                 },
               }}>
-              <Center>
-                <AspectRatio
-                  ratio={1 / 1}
-                  height="100%"
-                  bg={isEmergency ? emergencyBgStyle : nonEmergencyBgStyle}
-                  rounded="full">
-                  <Circle m={0.3} bg={MAIN.lightGrey} size="60%"></Circle>
-                </AspectRatio>
-              </Center>
+              <AspectRatio
+                ratio={1 / 1}
+                height="100%"
+                bg={isEmergency ? emergencyBgStyle : nonEmergencyBgStyle}
+                rounded="full">
+                <Center>
+                  <MaterialCommunityIcons
+                    name={
+                      isEmergency
+                        ? 'white-balance-sunny'
+                        : 'moon-waning-crescent'
+                    }
+                    size={35}
+                    color={MAIN.lightGrey}
+                  />
+                </Center>
+              </AspectRatio>
             </PresenceTransition>
           </HStack>
         </AspectRatio>
