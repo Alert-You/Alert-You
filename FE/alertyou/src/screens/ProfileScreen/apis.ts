dfd26adbcb18b8e701c82827f3fe01d683941b62
Sang Jin <tkdwls8412@naver.com>
2022-11-16 16:48:41 +0900

    fix: 타입 오류 수정

--- FE/alertyou/src/screens/ProfileScreen/apis.ts
+++ FE/alertyou/src/screens/ProfileScreen/apis.ts
@@ -25,19 +25,19 @@ export const getStudents = async (grade: any, schoolRoom: string): Promise<stude
   return result.data.students;
 };
 
-export const getStudent = async (studentId: number | undefined): Promise<studentType> => {
+export const getStudent = async (studentId: number | null | undefined): Promise<studentType> => {
   const result = await AxiosInstance.get(`teacher/student?studentId=${studentId}`)
     .then((response) => response);
   return result.data.student;
 };
 
-export const getReportList = async (studentId: number | undefined): Promise<repType[]> => {
+export const getReportList = async (studentId: number | null | undefined): Promise<repType[]> => {
   const result = await AxiosInstance.get(`report/list?userId=${studentId}`)
     .then((response) => response);
   return result.data.reports;
 }
 
-export const getProofList = async (studentId: number | undefined): Promise<proofType[]> => {
+export const getProofList = async (studentId: number | null | undefined): Promise<proofType[]> => {
   const result = await AxiosInstance.get(`proof?userId=${studentId}`)
     .then((response) => response);
   return result.data.proofs;
@@ -49,13 +49,16 @@ export const downloadProof = async (proofId: number | undefined): Promise<string
   return result.data
 }
 
-export const handleBodyguard = async (guardId: number | undefined) => {
+export const handleBodyguard = async (guardId: number | null | undefined) => {
   const result = await AxiosInstance.post(`bodyguard?guardId=${guardId}`)
-    .then((response) => response);
+    .then((response) => {
+      console.log(response.data)
+      return response;
+    });
   return true
 }
 
-export const excludeStudent = async (studentId: number | undefined) => {
+export const excludeStudent = async (studentId: number | null | undefined) => {
   const result = await AxiosInstance.delete(`/teacher/student?studentId=${studentId}`)
     .then((response) => response);
   return true
