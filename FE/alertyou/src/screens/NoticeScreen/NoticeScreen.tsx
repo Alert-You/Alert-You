c9137c0d386a25382e316705d34ed31ffe4112f8
Sang Jin <tkdwls8412@naver.com>
2022-11-17 16:23:11 +0900

    design: 디자인 수정

--- FE/alertyou/src/screens/NoticeScreen/NoticeScreen.tsx
+++ FE/alertyou/src/screens/NoticeScreen/NoticeScreen.tsx
@@ -1,10 +1,11 @@
-import { View, Text, Pressable } from 'react-native'
+import { View, Pressable, Image, Dimensions } from 'react-native'
 import React, { Suspense } from 'react'
-import { ScrollView } from 'native-base';
+import { Center, ScrollView, Text } from 'native-base';
 
 import { useIsFocused } from '@react-navigation/native';
 import { NoticeList } from '@/screens/NoticeScreen'
 import { useMutation, useQuery } from '@tanstack/react-query';
+import zeroNotice from '@assets/zeronotice.png'
 
 import { checkNoticeAll, getNoticeList } from './api';
 import { styles } from './style';
@@ -23,21 +24,31 @@ const NoticeScreen = ({ navigation }: Props) => {
     },
   })
 
-  const readAllNotive = () => {
+  const readAllNotice = () => {
     checkMutation.mutate()
   }
   return (
     <View style={styles.container}>
-      {data &&
-        <Pressable onPress={readAllNotive}>
-          <Text style={styles.readContainer}>모두 읽기</Text>
+      {data?.unRead.length !== 0 &&
+        <Pressable onPress={readAllNotice}>
+          <Text style={styles.readContainer}>모두 읽음 표시</Text>
         </Pressable>
       }
-      <Suspense >
-        <ScrollView>
-          <NoticeList noticeList={data?.unRead} read={false} />
-          <NoticeList noticeList={data?.read} read={true} />
-        </ScrollView>
+      <Suspense>
+        {data?.unRead.length !== 0 || data?.read.length !== 0 ?
+          <ScrollView>
+            <NoticeList noticeList={data?.unRead} read={false} />
+            <NoticeList noticeList={data?.read} read={true} />
+          </ScrollView>
+          :
+          <View style={{ flex: 1, justifyContent: 'center', marginBottom: 100 }}>
+            <Center>
+              <Image
+                source={zeroNotice}
+                style={{ width: Dimensions.get('window').width - 250, height: Dimensions.get('window').height - 500 }} />
+            </Center>
+          </View>
+        }
       </Suspense>
     </View>
   )
