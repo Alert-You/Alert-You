20f07c4c735707d484f477167d69a0207543fa0d
SeungHun Park <tmdgns1126@naver.com>
2022-11-19 17:38:02 +0900

    style: 홈 스크린 그라데이션 개선

--- FE/alertyou/src/theme/Home/gradient.ts
+++ FE/alertyou/src/theme/Home/gradient.ts
@@ -1,6 +1,53 @@
+// 원본
+// const emergencyColor = {
+//   light: '#f7b733',
+//   dark: '#fc4a1a',
+// };
+
+// https://uigradients.com/#Influenza
+// const emergencyColor = {
+//   light: '#C04848',
+//   dark: '#480048',
+// };
+
+// https://uigradients.com/#Peach
+const emergencyColor = {
+  light: '#FFEDBC',
+  dark: '#ED4264',
+};
+
+// https://uigradients.com/#SweetMorning
+// const emergencyColor = {
+//   light: '#FFC371',
+//   dark: '#FF5F6D',
+// };
+
+// 원본
+// const nonEmergencyColor = {
+//   light: '#85D8CE',
+//   dark: '#085078',
+// };
+
+// https://uigradients.com/#Mantle
+const nonEmergencyColor = {
+  light: '#24C6DC',
+  dark: '#514A9D',
+};
+
+// https://uigradients.com/#AquaMarine
+// const nonEmergencyColor = {
+//   light: '#26D0CE',
+//   dark: '#1A2980',
+// };
+
+const reportBtnColor = {
+  light: '#ffffff',
+  dark: '#24C6DC',
+};
+
 export const emergencyBgStyle = {
   linearGradient: {
-    colors: ['#ff7e5f', '#feb47b'],
+    colors: [emergencyColor.dark, emergencyColor.light],
     start: [0, 0],
     end: [1, 1],
   },
@@ -8,7 +55,7 @@ export const emergencyBgStyle = {
 
 export const nonEmergencyBgStyle = {
   linearGradient: {
-    colors: ['#044461', '#74bbdb'],
+    colors: [nonEmergencyColor.dark, nonEmergencyColor.light],
     start: [0, 0],
     end: [1, 1],
   },
@@ -16,7 +63,7 @@ export const nonEmergencyBgStyle = {
 
 export const EmergencyInnerBtnStyle = {
   linearGradient: {
-    colors: ['#4ca2cd', '#000428'],
+    colors: [emergencyColor.dark, emergencyColor.light],
     start: [0, 0],
     end: [1, 1],
   },
@@ -24,7 +71,7 @@ export const EmergencyInnerBtnStyle = {
 
 export const EmergencyOuterBtnStyle = {
   linearGradient: {
-    colors: ['#4ca2cd', '#000428'],
+    colors: [emergencyColor.light, emergencyColor.dark],
     start: [0, 0],
     end: [1, 1],
   },
@@ -32,7 +79,7 @@ export const EmergencyOuterBtnStyle = {
 
 export const nonEmergencyInnerBtnStyle = {
   linearGradient: {
-    colors: ['#044461', '#74bbdb'],
+    colors: [nonEmergencyColor.dark, nonEmergencyColor.light],
     start: [0, 0],
     end: [1, 1],
   },
@@ -40,7 +87,7 @@ export const nonEmergencyInnerBtnStyle = {
 
 export const nonEmergencyOuterBtnStyle = {
   linearGradient: {
-    colors: ['#74bbdb', '#044461'],
+    colors: [nonEmergencyColor.light, nonEmergencyColor.dark],
     start: [0, 0],
     end: [1, 1],
   },
@@ -48,7 +95,7 @@ export const nonEmergencyOuterBtnStyle = {
 
 export const reportBtnStyle = {
   linearGradient: {
-    colors: ['#ffffff', '#518bc9'],
+    colors: [reportBtnColor.light, reportBtnColor.dark],
     start: [0, 0],
     end: [1, 1],
   },
