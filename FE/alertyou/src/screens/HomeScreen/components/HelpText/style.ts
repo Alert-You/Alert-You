a79bb06699472418bdeed7d6998bf21796f627a5
SeungHun Park <tmdgns1126@naver.com>
2022-11-19 22:49:07 +0900

    style: 홈화면 이용안내 문구 UI 개선

--- FE/alertyou/src/screens/HomeScreen/components/HelpText/style.ts
+++ FE/alertyou/src/screens/HomeScreen/components/HelpText/style.ts
@@ -1,7 +1,28 @@
-import { Dimensions, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { W } from '@/constants/dimensions';
+import { emergencyColor } from '@/theme/Home/gradient';
 
 export const styles = StyleSheet.create({
-  helpText: {
-    backgroundColor: 'transparent',
+  container: {
+    paddingHorizontal: 48,
+    paddingVertical: 24,
+    maxWidth: W - 32,
+    borderRadius: 12,
+    borderColor: emergencyColor.light,
+    borderWidth: 1,
+    elevation: 3,
+  },
+
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+    marginBottom: 12,
+  },
+
+  paragraph: {
+    fontSize: 15,
+    color: 'white',
+    textAlign: 'center',
   },
 });
